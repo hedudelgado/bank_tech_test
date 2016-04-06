0af1570fedaaabbcac3f18e8f9cc47467750233d
hedudelgado <hedu.delgado@gmail.com>
2016-04-06 11:34:14 +0100

    you can make deposit test passing

--- lib/BankAccount.rb
+++ lib/BankAccount.rb
@@ -6,7 +6,15 @@ class BankAccount
 		@balance = 0
 	end
 
-
-	def make_deposit
+	def make_deposit(amount)
+		increase_balance(amount)
 	end
+
+
+	private
+
+	def increase_balance(amount)
+		@balance = @balance + amount
+	end
+
 end
